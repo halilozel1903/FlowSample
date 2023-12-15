f593f7df5dd95f4fb64d2e82a5e1f534cd4dc01d
Halil Ozel <halilozel1903@gmail.com>
2023-12-15 20:59:39 +0300

    StateFlow, SharedFlow, LiveData samples added.

--- app/src/main/java/com/halil/ozel/flowsample/FlowViewModel.kt
+++ app/src/main/java/com/halil/ozel/flowsample/FlowViewModel.kt
@@ -1,8 +1,15 @@
 package com.halil.ozel.flowsample
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
@@ -58,6 +65,32 @@ class FlowViewModel : ViewModel() {
                 delay(2000)
                 println("counter is : $it")
             }
+        }
+    }
+
+    // LiveData vs StateFlow vs SharedFlow
+
+    private val  _liveData = MutableLiveData("Kotlin LiveData")
+    val liveData: LiveData<String> = _liveData
+
+    fun changeLiveDataValue() {
+        _liveData.value = "Live Data"
+    }
+
+    private val  _stateFlow = MutableStateFlow("Kotlin StateFlow")
+    val stateFlow = _stateFlow.asStateFlow()
+
+    fun changeStateFlowValue() {
+        _stateFlow.value = "State Flow"
+    }
+
+    // SharedFlow is highly configurable version of stateFlow.
+    private val  _sharedFlow = MutableSharedFlow<String>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+    fun changeSharedFlowValue() {
+        viewModelScope.launch {
+            _sharedFlow.emit("Shared Flow")
         }
     }
 }
--- app/src/main/java/com/halil/ozel/flowsample/MainActivity.kt
+++ app/src/main/java/com/halil/ozel/flowsample/MainActivity.kt
@@ -5,14 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.halil.ozel.flowsample.ui.theme.FlowSampleTheme
 
@@ -22,17 +29,75 @@ class MainActivity : ComponentActivity() {
         setContent {
             FlowSampleTheme {
                 val flowViewModel: FlowViewModel by viewModels()
-                val counter = flowViewModel.countDownTimerFlow.collectAsState(initial = 13)
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-                ) {
-                    Box(modifier = Modifier.fillMaxSize()) {
-                        Text(
-                            text = counter.value.toString(),
-                            fontSize = 24.sp,
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier.align(Alignment.Center)
-                        )
+                SecondScreen(flowViewModel = flowViewModel)
+            }
+        }
+    }
+
+    @Composable
+    fun FirstScreen(flowViewModel: FlowViewModel) {
+        val counter = flowViewModel.countDownTimerFlow.collectAsState(initial = 13)
+        Surface(
+            modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Text(
+                    text = counter.value.toString(),
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun SecondScreen(flowViewModel: FlowViewModel) {
+
+        val liveDataValue = flowViewModel.liveData.observeAsState()
+
+        val stateFlowValue = flowViewModel.stateFlow.collectAsState()
+
+        val sharedFlowValue = flowViewModel.sharedFlow.collectAsState(initial = "")
+
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Column(modifier = Modifier.align(Alignment.Center)) {
+                    Text(
+                        text = liveDataValue.value ?: "",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Button(onClick = {
+                        flowViewModel.changeLiveDataValue()
+                    }) {
+                        Text(text = "LiveData Button")
+                    }
+                    Spacer(modifier = Modifier.padding(10.dp))
+
+                    Text(
+                        text = stateFlowValue.value ?: "",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Button(onClick = {
+                        flowViewModel.changeStateFlowValue()
+                    }) {
+                        Text(text = "State Flow Button")
+                    }
+
+                    Spacer(modifier = Modifier.padding(10.dp))
+
+                    Text(
+                        text = sharedFlowValue.value,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Button(onClick = {
+                        flowViewModel.changeSharedFlowValue()
+
+                    }) {
+                        Text(text = "Shared Flow Button")
                     }
                 }
             }
