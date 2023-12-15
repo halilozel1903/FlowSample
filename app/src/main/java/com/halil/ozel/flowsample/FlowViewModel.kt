d3c12e56624be4a01a9f03bce2259872ee8c5b9b
Halil Ozel <halilozel1903@gmail.com>
2023-12-15 20:09:23 +0300

    Flow Operators Sample Added.

--- app/src/main/java/com/halil/ozel/flowsample/FlowViewModel.kt
+++ app/src/main/java/com/halil/ozel/flowsample/FlowViewModel.kt
@@ -1,8 +1,12 @@
 package com.halil.ozel.flowsample
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
 /**
  * Created by Halil Ozel on 15.12.2023.
@@ -12,10 +16,28 @@ class FlowViewModel : ViewModel() {
         val countDownFrom = 13
         var counter = countDownFrom
         emit(countDownFrom)
-        while (counter > 0){
+        while (counter > 0) {
             delay(1000)
-            counter --
+            counter--
             emit(counter)
+        }
+    }
+
+    init {
+        collectInViewModel()
+    }
+
+    private fun collectInViewModel() {
+        viewModelScope.launch {
+            countDownTimerFlow.filter {
+                it % 4 == 0
+            }
+                .map {
+                    it * it
+                }
+                .collect {
+                    println("counter is : $it")
+                }
         }
     }
 }
